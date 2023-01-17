e58a33645c53850cca290d240867fbe8dee34f11
BeeMOre32 <windowssart01@gmail.com>
2023-01-17 21:42:29 +0900

    feat: add loading animation and loading handler

--- js/main.js
+++ js/main.js
@@ -3,10 +3,13 @@ import { renderToDo } from "./renderToDo";
 
 const toDoInputEl = document.querySelector(".create-todo__input");
 const createToDoBtn = document.querySelector(".create-todo__button");
+const toDoListEl = document.querySelector(".todo-list__list");
+const doneListEl = document.querySelector(".done-list__list");
 
 let inputValue = "";
 
 window.onload = async () => {
+  handlingLoading();
   const data = await fetchToDo();
   renderToDo(data);
 };
@@ -17,11 +20,15 @@ toDoInputEl.addEventListener("input", (e) => {
 
 toDoInputEl.addEventListener("keydown", async (e) => {
   if (e.key === "Enter" && !e.isComposing) {
+    toDoListEl.innerHTML = "";
+    doneListEl.innerHTML = "";
+    handlingLoading();
     await handleCreateToDo(inputValue);
   }
 });
 
 createToDoBtn.addEventListener("click", async () => {
+  handlingLoading();
   await handleCreateToDo(inputValue);
 });
 
@@ -45,3 +52,16 @@ toDoInputEl.addEventListener("blur", () => {
   toDoInputEl.classList.remove("active");
   createToDoSpanEl.classList.remove("active");
 });
+
+export function handlingLoading() {
+  const toDoListEl = document.querySelector(".todo-list__list");
+  const doneListEl = document.querySelector(".done-list__list");
+  const loadingEl = document.createElement("li");
+  loadingEl.classList.add("loading");
+  loadingEl.innerHTML = `<span></span>`;
+
+  for (let i = 0; i < 2; i++) {
+    toDoListEl.appendChild(loadingEl.cloneNode(true));
+    doneListEl.appendChild(loadingEl.cloneNode(true));
+  }
+}
